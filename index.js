62ecc7b283d3f3198c58aaa97d14bcefb0d308a2
dmitriy365377 <tigr365377@gmail.com>
2020-02-15 20:23:42 +0300

    unique-values

--- index.js
+++ index.js
@@ -925,4 +925,48 @@ function palindrome(str) {
 }
 
 
-console.log(palindrome(str));
+// console.log(palindrome(str));
+
+
+
+// Count Unique Values 
+
+// Implementing a function called countUniqueValues
+// which accepts a sorted array, and counts the 
+// unique values in the array. There can be negative 
+// numbers in the array, but it will be sorted 
+
+
+function countUniqueValues(arr) {
+  if (arr.length === 0) return 0
+  let i = 0
+  for (j = 1; j < arr.length ; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j]
+    }
+    
+  }
+  return i + 1 
+}
+
+//console.log(countUniqueValues([1, 1, 1, 1, 2])) // 2 unique values  
+
+// да я даже не решал
+//                         ( (.)(.) )  
+
+// Можно еще так наверно ща поппробуем
+
+// function test(arr) {
+//   const obj = {};
+//   const arr = [];
+//   for (const prop of arr) {
+//     if (!obj[prop]) {
+//       obj[prop] = 1;
+//       arr.push(prop)
+//     } else {
+//       obj[prop]++;
+//     }
+//   }
+//   return arr.length;
+// }
