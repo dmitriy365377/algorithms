0d05529f7b96d169f230605901428e8f815b38ed
dmitriy365377 <tigr365377@gmail.com>
2020-06-30 19:39:34 +0300

    promises

--- promise.js
+++ promise.js
@@ -4,6 +4,8 @@
 //   }, Math.random() * 2000);
 // }
 
+const { reject } = require("ramda");
+
 // function sumPromise(a, b) {
 //   return new Promise((resolve, reject) => {
 //     setTimeout(() => {
@@ -23,46 +25,49 @@
 //   reason => {
 //     console.log('err', reason);
 //   },
-// )
+// // )
 
-const p1 = new Promise(r => setTimeout(r, Math.random() * 2000, 6));
-const p2 = new Promise(r => setTimeout(r, Math.random() * 2000, [1,2]));
-const p3 = new Promise(r => setTimeout(r, Math.random() * 2000, 'hello'));
-const p4 = new Promise(r => setTimeout(r, Math.random() * 2000, {z: 1}));
-const p5 = new Promise(r => setTimeout(r, Math.random() * 2000, () => {}));
+// const p1 = new Promise(r => setTimeout(r, Math.random() * 2000, 6));
+// const p2 = new Promise(r => setTimeout(r, Math.random() * 2000, [1,2]));
+// const p3 = new Promise(r => setTimeout(r, Math.random() * 2000, 'hello'));
+// const p4 = new Promise((res, rej) => setTimeout(rej, Math.random() * 2000, {z: 1}));
+// const p5 = new Promise(r => setTimeout(r, Math.random() * 2000, () => {}));
 
-// p1.then(x => {
+// // p1.then(x => {
 
-// })
+// // })
 
-function all(promises) {
-   return new Promise((resolve)=> {
-      const arr = []
-      let count = 0
-      for(let i = 0; i < promises.length; i++) {
-        promises[i].then(
-            value => {
-                arr[i] = value
-                count++;
-                if (count === promises.length) {
-                    resolve(arr)
-                }
-            }
-        )
-      }
-   })
-}
+// function all(promises) {
+//    return new Promise((resolve)=> {
+//       const arr = []
+//       let count = 0
+//       for(let i = 0; i < promises.length; i++) {
+//         promises[i].then(
+//             value => {
+//                 arr[i] = value
+//                 count++;
+//                 if (count === promises.length) {
+//                     resolve(arr)
 
-// Promise.all 
-// Promise.race 
-// Promise.allSettled 
-// Promise.any 
+//                 }
+//             },
+//             reject
+//         )
+//       }
+//    })
+// }
 
-all([p1, p2, p3, p4, p5]).then(
-    value => {
-        console.log(value)
-    }
-) // [1, 2, 3, 4, 5]
+// // Promise.all 
+// // Promise.race 
+// // Promise.allSettled 
+// // Promise.any 
+
+// all([p1, p2, p3, p4, p5]).then(
+//     value => {
+//         console.log(value)
+//     },
+//     err => console.log('err:', err) // err: {z: 1}
+// ) // [1, 2, 3, 4, 5]
 
 
 // p1.then(console.log);
@@ -71,3 +76,90 @@ all([p1, p2, p3, p4, p5]).then(
 // p4.then(console.log);
 // p5.then(console.log);
 
+
+
+
+// function race(promises) {
+//     return new Promise((resolve, reject) => {
+//         for (let i = 0; i < promises.length; i++) {
+//             promises[i].then(resolve, reject);
+//         }
+//     })
+// }
+
+// race([p1, p2, p3, p4, p5]).then(
+//     value => {
+//         console.log(value)
+//     },
+//     err => { console.log(err) }
+// );
+
+
+const p1 = new Promise((res, rej) => setTimeout(rej, Math.random() * 2000, 6));
+const p2 = new Promise((res, rej) => setTimeout(rej, Math.random() * 2000, [1,2]));
+const p3 = new Promise((res, rej) => setTimeout(rej, Math.random() * 2000, 'hello'));
+const p4 = new Promise((res, rej) => setTimeout(rej, Math.random() * 2000, {z: 1}));
+const p5 = new Promise((res, rej) => setTimeout(rej, Math.random() * 2000, () => {}));
+
+// // Promise.allSettled([p1, p2, p3, p4, p5]).then(console.log)
+    
+// function allSettled(promises) {
+//     return new Promise((resolve, reject) => {
+//         const arr = []
+//         let count = 0
+//         for (let i = 0; i < promises.length; i++) {
+//             promises[i]
+//                 .then(
+//                     value => ({ status: "fulfilled", value }),
+//                     reason => ({ status: "rejected", reason }),
+//                 )
+//                 .then(data => {
+//                     arr[i] = data;
+//                     count++;
+//                     if (count === promises.length) {
+//                         resolve(arr)
+//                     }
+//                 });
+//         }
+//     })
+// }
+
+// allSettled([p1, p2, p3, p4, p5]).then(
+//     value => {
+//         console.log(value)
+//     }
+// );
+
+
+
+
+function any(promises) {
+    return new Promise((resolve, reject) => {
+        let rejectCount = 0
+        for (let i = 0; i < promises.length; i++) {
+            promises[i].then(
+                value => {
+                    resolve(value)
+                },
+                () => {
+                    rejectCount++
+                    if(promises.length === rejectCount) {
+                        reject(new Error("No Promise in Promise.any was resolved"))
+                    }
+                }
+            )
+                
+        }
+    })
+}
+
+
+
+any([p1, p2, p3, p4, p5]).then(
+    value => {
+        console.log('ok', value)
+    },
+    error => {
+        console.log('err', error)
+    }
+);
